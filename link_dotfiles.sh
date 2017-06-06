8488af7ede6809740d298966d12eafd58e08fbe2
octarect <ryo.alter@gmail.com>
2017-06-06 14:48:19 +0900

    reconstruct link_dotfiles.sh

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,2 +1,26 @@
 #! /bin/bash
-ln -s ~/dotfiles/.vimrc ~/.vimrc
+
+function log() {
+  echo "[$1] $2"
+}
+
+DIR=`dirname $0`
+if [ -n $XDG_CONFIG_HOME ]; then
+  conf_dir=${HOME}/.config
+fi
+
+# vim
+vimrc=${HOME}/.vimrc
+if [ ! -e ${vimrc} ]; then
+  ln -s ${DIR}/.vimrc ${HOME}/.vimrc
+else
+  log 'WARN' "the file ${vimrc} already exists, and not linked."
+fi
+
+# neovim
+nvim_dir=${conf_dir}/nvim
+if [ ! -e ${nvim_dir} ]; then
+  ln -s ${DIR}/.config/nvim ${nvim_dir}
+else
+  log 'WARN' "the directory ${nvim_dir} already exists, and not linked."
+fi
