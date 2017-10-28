5e5c353faf9cca61b26280311f47d439ea706a09
octarect <ryo.alter@gmail.com>
2017-10-28 22:24:56 +0900

    ghg to manage remote packages on gh-r

--- inst/go/make.sh
+++ inst/go/make.sh
@@ -1,20 +1,27 @@
 #!/bin/sh
 
 RM="rm -rf"
+EXE="/bin/sh -cx"
 
 dir=$(cd $(dirname $0) && pwd)
 repo_dir=${dir}/../..
+. ${repo_dir}/sh/util.sh
+
+gopath=$(go env GOPATH)
 
 if [ "$1" = "install" ]; then
-  go get github.com/motemen/ghq
+  ${EXE} "go get github.com/motemen/ghq"
+  ${EXE} "go get github.com/Songmu/ghg/cmd/ghg"
+  ghgpath=$(ghg bin)
 
-  ghq get peco/peco
+  ${EXE} "ghg get peco/peco"
+  ${EXE} "mv ${ghgpath}/peco ${gopath}/bin/peco"
+
 elif [ "$1" = "clean" ]; then
-  gopath=$(go env GOPATH)
+  ${EXE} "${RM} ${gopath}/bin/peco"
 
-  ${RM} $(ghq list -p -e peco)
-  ${RM} ${gopath}/bin/peco
-
-  ${RM} $(ghq list -p -e ghq)
-  ${RM} ${gopath}/bin/ghq
+  ${EXE} "${RM} $(ghq list -p -e ghg)"
+  ${EXE} "${RM} ${gopath}/bin/ghg"
+  ${EXE} "${RM} $(ghq list -p -e ghq)"
+  ${EXE} "${RM} ${gopath}/bin/ghq"
 fi
