7f4e11067c8060048a044149a753088c54a7c141
octarect <ryo.alter@gmail.com>
2018-04-22 19:45:52 +0900

    fix hook

--- inst/git-hooks/make.sh
+++ inst/git-hooks/make.sh
@@ -12,14 +12,14 @@ git_hook_dir="${repo_dir}/.git/hooks"
 hooks=$(ls -1 ${dir} | grep -E "^(pre|post|apply|commit|update)-")
 
 if [ "$1" = "install" ]; then
-  echo -e $hooks | while read hook
+  echo "${hooks}" | while read hook
   do
     cmd="${CP} ${dir}/${hook} ${git_hook_dir}/${hook}"
     log INFO "${cmd}"
     ${cmd}
   done
 elif [ "$1" = "clean" ]; then
-  echo -e $hooks | while read hook
+  echo "${hooks}" | while read hook
   do
     cmd="${RM} ${git_hook_dir}/${hook}"
     log INFO "${cmd}"
