5cbccf90623d91e6a836a596f8203367f41f059c
octarect <ryo.alter@gmail.com>
2018-04-22 13:26:50 +0900

    refactor

--- make.sh
+++ make.sh
@@ -7,7 +7,7 @@ REPO_DIR=$(cd $(dirname $0) && pwd)
 # load convenient functions
 . ${REPO_DIR}/sh/util.sh
 
-specific() {
+makesh() {
   cmd=$1
   op=$2
   script=${REPO_DIR}/inst/${cmd}/make.sh
@@ -27,12 +27,12 @@ else
 fi
 
 if [ ! -z $2 ]; then
-  specific $2 $op
+  makesh $2 $op
   exit 0
 fi
 
 # install or clean configulations in $XDG_CONFIG_HOME
-specific config $op
+makesh config $op
 
 # execute command-specific installations
 targets=$(find ${REPO_DIR}/inst/* -maxdepth 0 -type d)
@@ -40,6 +40,6 @@ for target in $targets
 do
   cmd=$(basename $target)
   if has $cmd; then
-    specific $cmd $op
+    makesh $cmd $op
   fi
 done
