f4b09a3412acbe14f67a7b8de99a9be2a7ec83c3
Ryota Kota <ryota.kota@member.fsf.org>
2019-02-03 03:23:58 +0900

    fix bat plugin installation

--- config/zsh/lib/zplug/plugins_linux.zsh
+++ config/zsh/lib/zplug/plugins_linux.zsh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
 
-zplug "sharkdp/bat", from:gh-r, as:command, use:"*x86_64-unknown-linux-gnu*", rename-to:bat
+zplug "sharkdp/bat", from:gh-r, use:"*x86_64*linux-gnu*", as:command, rename-to:bat
