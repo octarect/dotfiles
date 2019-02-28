607b050ef43a00906783b051547651d3f77519dc
Ryota Kota <ryota.kota@member.fsf.org>
2019-03-01 03:54:02 +0900

    disable zplug self-manage

--- config/zsh/lib/zplug/plugins.zsh
+++ config/zsh/lib/zplug/plugins.zsh
@@ -7,7 +7,7 @@ fi
 
 source $ZPLUG_HOME/init.zsh
 
-zplug "zplug/zplug", hook-build:"zplug --self-manage"
+# zplug "zplug/zplug", hook-build:"zplug --self-manage"
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "zsh-users/zsh-history-substring-search"
 
