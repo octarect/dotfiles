173b34672046728c423cd9eb72561742f6cc5110
nbitmage <ryota.kota@member.fsf.org>
2019-04-06 19:55:09 +0900

    add init script (should be run only once on each system)

---
+++ etc/init.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+
+run_init_scripts() {
+    echo "aaa"
+}
+
+init_scripts() {
+    local init_script_dir="${DOT_PATH}/etc/init"
+    echo $(find ${init_script_dir} -mindepth 1 -maxdepth 1 -type f)
+}
+
+source_all() {
+    local script_paths="$1"
+    for file in "$(init_scripts)"
+    do
+        source "${file}"
+    done
+}
+
+if [ -z ${DOT_PATH} ]; then
+    echo '$DOT_PATH is required.' >&2
+    exit 1
+fi
+
+source ${DOT_PATH}/src/dotlib/init.sh
+
+source_all $(init_scripts)
---
+++ etc/init/01_nix.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sh <(curl https://nixos.org/nix/install)
+
+source ${HOME}/.nix-profile/etc/profile.d/nix.sh
