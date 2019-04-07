3a165ee8339142b6f753ae4334f81c25409a033b
nbitmage <ryota.kota@member.fsf.org>
2019-04-08 02:17:26 +0900

    move dotfiles to home dir

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -9,9 +9,17 @@ deploy() {
 }
 
 XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config}"
-DOT_DEPLOY_PATH=${DOT_PATH}/etc/deploy
+
+DOT_DEPLOY_PATH=${DOT_PATH}/home
 DOT_CONFIG_PATH=${DOT_DEPLOY_PATH}/config
-DOT_ZSHENV_PATH=${DOT_DEPLOY_PATH}/config/zsh/.zshenv
+
+########################################
+# files directly under $HOME
+########################################
+for src_path in $(find ${DOT_DEPLOY_PATH} -maxdepth 1 -type f)
+do
+    deploy ${src_path} ${HOME}/$(basename ${src_path})
+done
 
 ########################################
 # .config
@@ -23,8 +31,3 @@ do
     dst_path=${XDG_CONFIG_HOME}/${conf_name}
     deploy ${src_path} ${dst_path}
 done
-
-########################################
-# .zshenv
-########################################
-deploy ${DOT_ZSHENV_PATH} ${HOME}/.zshenv
