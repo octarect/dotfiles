3b889d8d82294d423dff58ce677d74d454508e9e
nbitmage <ryota.kota@member.fsf.org>
2019-04-08 00:46:01 +0900

    anyenv may have to exist ~/.anyenv in order that pyenv-virtualenv works correctly.

--- etc/init/10_anyenv.sh
+++ etc/init/10_anyenv.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash -e
 
 ANYENV_GIT_REPO=https://github.com/riywo/anyenv
-ANYENV_PATH=${DOT_TMP_PATH}/.anyenv
 
-if [ ! -e ${ANYENV_PATH} ]; then
-    git clone ${ANYENV_GIT_REPO} ${ANYENV_PATH}
+anyenv_path=${HOME}/.anyenv
+anyenv_bin=${anyenv_path}/bin/anyenv
+
+if [ ! -e ${anyenv_path} ]; then
+    git clone ${ANYENV_GIT_REPO} ${anyenv_path}
 fi
+
+if [ ! -e ${HOME}/.config/anyenv/anyenv-install ]; then
+    ${anyenv_bin} install --init
+fi
+
+eval "$(${anyenv_bin} init - --no-rehash)"
+
+${anyenv_bin} install pyenv
+${anyenv_bin} install rbenv
