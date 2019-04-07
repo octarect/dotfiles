9c98c84bbfe795f957ae50fd05a2a3e722826f40
nbitmage <ryota.kota@member.fsf.org>
2019-04-08 01:02:47 +0900

    stop using nix

--- etc/init/05_nix.sh
+++ etc/init/05_nix.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-sh <(curl https://nixos.org/nix/install)
+# sh <(curl https://nixos.org/nix/install)
 
-source ${DOT_PATH}/src/dotlib/init.sh
-__dotlib::load_nix
+# source ${DOT_PATH}/src/dotlib/init.sh
+# __dotlib::load_nix
 
-nix-channel --update
+# nix-channel --update
