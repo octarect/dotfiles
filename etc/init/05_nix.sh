7018ff8cbc159cf9f82243321831ff5076a7a259
nbitmage <ryota.kota@member.fsf.org>
2019-04-12 14:37:43 +0900

    add tmux

--- etc/init/05_nix.sh
+++ etc/init/05_nix.sh
@@ -8,5 +8,6 @@ source ${DOT_PATH}/lib/dotlib/init.sh
 __dotlib::load_nix
 
 nix-channel --update
+nix-env --install tmux
 nix-env --install neovim
 nix-env --install neofetch
---
+++ etc/init/50_tmux.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+TMUX_PLUGINS_DIR="${DOT_LOCAL_DIR}/tmux/plugins"
+TPM_REPO_URL="https://github.com/tmux-plugins/tpm"
+
+mkdir -p ${TMUX_PLUGINS_DIR}
+
+git clone ${TPM_REPO_URL} ${TMUX_PLUGINS_DIR}/tpm
