2035525a2ff7204a8df16fdc96de0ee7f44e2790
nbitmage <ryota.kota@member.fsf.org>
2019-04-14 17:27:39 +0900

    install python 3.6.x

--- etc/init/10-anyenv.sh
+++ etc/init/10-anyenv.sh
@@ -13,4 +13,22 @@ if [ ! -e ${HOME}/.config/anyenv/anyenv-install ]; then
     yes | ${anyenv_bin} install --init
 fi
 
-eval "$(${anyenv_bin} init - --no-rehash)"
+eval "$(${anyenv_bin} init -)"
+
+if ! anyenv versions | grep pyenv >/dev/null; then
+  anyenv install pyenv
+else
+  __dotlib::log info "pyenv is already installed."
+fi
+
+if ! anyenv versions | grep rbenv >/dev/null; then
+  anyenv install rbenv
+else
+  __dotlib::log info "rbenv is already installed."
+fi
+
+eval "$(${anyenv_bin} init -)"
+
+py36latest=$(pyenv install --list | grep -E "^\s*3.6.[0-9]$" | tail -n1 | sed 's/\s\s*//')
+pyenv install ${py36latest}
+pyenv global ${py36latest}
