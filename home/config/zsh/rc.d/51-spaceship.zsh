d1aadb30a6dda5133f749f50fc6797d7a450ff17
Ryota Kota <ryota.kota@member.fsf.org>
2019-06-10 23:52:54 +0900

    remove unneeded component for spaceship

--- home/config/zsh/rc.d/51-spaceship.zsh
+++ home/config/zsh/rc.d/51-spaceship.zsh
@@ -18,24 +18,24 @@ SPACESHIP_PROMPT_ORDER=(
 	ruby          # Ruby section
 	elixir        # Elixir section
 	# xcode         # Xcode section
-	swift         # Swift section
-	golang        # Go section
+	# swift         # Swift section
+	# golang        # Go section
 	php           # PHP section
-	rust          # Rust section
-	haskell       # Haskell Stack section
-	julia         # Julia section
+	# rust          # Rust section
+	# haskell       # Haskell Stack section
+	# julia         # Julia section
 	docker        # Docker section
 	# aws           # Amazon Web Services section
-	venv          # virtualenv section
+	# venv          # virtualenv section
 	# conda         # conda virtualenv section
 	pyenv         # Pyenv section
 	# dotnet        # .NET section
 	# ember         # Ember.js section
-	kubecontext   # Kubectl context section
+	# kubecontext   # Kubectl context section
 	terraform     # Terraform workspace section
 	exec_time     # Execution time
 	line_sep      # Line break
-	battery       # Battery level and status
+	# battery       # Battery level and status
 	vi_mode       # Vi-mode indicator
 	jobs          # Background jobs indicator
 	exit_code     # Exit code section
