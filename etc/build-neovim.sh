bfecdc85438edafe1b8ff86574613df15ad00d1f
Ryota Kota <ryota.kota@member.fsf.org>
2019-06-23 00:11:46 +0900

    update build-neovim.sh

--- etc/build-neovim.sh
+++ etc/build-neovim.sh
@@ -2,42 +2,41 @@
 
 set -e
 
-NEOVIM_BUILD_DIR="${DOT_LOCAL_DIR}/neovim-build"
-CUR_DIR="$(pwd)"
+NVIM_PREFIX="${HOME}/.local"
+NVIM_BUILD_TMP="/tmp/nvim-build-$(date "+%Y%m%d%H%M%S")"
 
-git clone https://github.com/neovim/neovim ${NEOVIM_BUILD_DIR}
-cd ${NEOVIM_BUILD_DIR}
-make CMAKE_BUILD_TYPE=Release
-make
-mv ./build/bin/nvim ${DOT_LOCAL_DIR}/bin/nvim
-cd ${CUR_DIR}
-rm -rf ${NEOVIM_BUILD_DIR}
-
-# Use anyenv
-eval "$(${anyenv_bin} init -)"
+cur_dir="$(pwd)"
+git clone https://github.com/neovim/neovim ${NVIM_BUILD_TMP}
+cd ${NVIM_BUILD_TMP}
+make CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=${NVIM_PREFIX}"
+make install
+cd ${cur_dir}
+rm -rf ${NVIM_BUILD_TMP}
 
 ########################################
 # Provider: python
 ########################################
+
+PY2_VENV_NAME=nvim-python2
+PY3_VENV_NAME=nvim-python3
+
+# Use anyenv
+eval "$(${HOME}/.anyenv/bin/anyenv init -)"
+
 pylist="$(pyenv install --list | sed -e 's/[[:space:]]//g')"
-py2latest=$(echo -n "${pylist}" | grep -E "^2(\.[0-9]+)+$" | tail -n1)
-py3latest=$(echo -n "${pylist}" | grep -E "^3.7.[0-9]$" | tail -n1)
 
-pyenv install ${py2latest}
-pyenv virtualenv ${py2latest} neovim-python2
-pyenv shell neovim-python2
-pip install pynvim
+if ! pyenv versions | grep ${PY2_VENV_NAME} 2>&1 1>/dev/null; then
+  py2latest=$(echo -n "${pylist}" | grep -E "^2(\.[0-9]+)+$" | tail -n1)
+  pyenv install ${py2latest}
+  pyenv virtualenv ${py2latest} ${PY2_VENV_NAME}
+  pyenv shell ${PY2_VENV_NAME}
+  pip install pynvim
+fi
 
-pyenv install ${py3latest}
-pyenv virtualenv ${py3latest} neovim-python3
-pyenv shell neovim-python3
-pip install pynvim
-
-########################################
-# Provider: ruby
-########################################
-NEOVIM_RUBY_VERSION=2.6.3
-rbenv install ${NEOVIM_RUBY_VERSION}
-rbenv shell ${NEOVIM_RUBY_VERSION}
-rbenv rehash
-gem install neovim
+if ! pyenv versions | grep ${PY3_VENV_NAME} 2>&1 1>/dev/null; then
+  py3latest=$(echo -n "${pylist}" | grep -E "^3.7.[0-9]$" | tail -n1)
+  pyenv install ${py3latest}
+  pyenv virtualenv ${py3latest} ${PY3_VENV_NAME}
+  pyenv shell ${PY3_VENV_NAME}
+  pip install pynvim
+fi
