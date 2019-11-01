0a576a1f10adf88c4d7b27b109561a39eafecbf8
nbitmage <ryota.kota@member.fsf.org>
2019-11-01 14:18:37 +0900

    fix method of downloading for starship in zplugin settings

--- zsh/rc.d/50-zplugin.zsh
+++ zsh/rc.d/50-zplugin.zsh
@@ -108,5 +108,5 @@ zplugin ice wait'0' as"program" from"gh" mv"gdown.pl -> gdown"
 zplugin load circulosmeos/gdown.pl
 
 # Load prompt.
-zplugin ice as'program' from'gh-r' atload'eval $(starship init zsh)' ver'v0.25.0'
+zplugin ice as'program' from'gh-r' mv'target/*/release/starship -> starship' atload'eval $(starship init zsh)'
 zplugin light starship/starship
