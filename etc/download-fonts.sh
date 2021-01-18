aee1414f3a42d23d1fb1de03c993b4ed9343d77f
Ryota Kota <ryota.kota@member.fsf.org>
2021-01-18 11:18:02 +0900

    font downloader supports darwin

--- etc/download-fonts.sh
+++ etc/download-fonts.sh
@@ -3,9 +3,14 @@
 set -xe
 
 CICA_RELEASES_URL="https://api.github.com/repos/miiton/Cica/releases"
+if [ "$(uname)" = "Darwin" ]; then
+    cica_font_path="${HOME}/Library/Fonts"
+else
+    cica_font_path="${HOME}/.local/share/fonts"
+fi
 
 curl -sfL "${CICA_RELEASES_URL}" | jq -r '.[0].assets | .[].browser_download_url' | grep with_emoji.zip | xargs -I{} curl -fL -o cica.zip "{}"
-unzip -d ~/.local/share/fonts cica.zip Cica-Regular.ttf Cica-RegularItalic.ttf Cica-Bold.ttf Cica-BoldItalic.ttf
+unzip -d $cica_font_path cica.zip Cica-Regular.ttf Cica-RegularItalic.ttf Cica-Bold.ttf Cica-BoldItalic.ttf
 rm cica.zip
 
 fc-cache -v
