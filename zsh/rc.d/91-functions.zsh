e424102b6317bcf5606a5cc9c436d4656c46108b
Ryota Kota <ryota.kota@member.fsf.org>
2021-03-10 16:33:32 +0900

    fix indent in 91-function.zsh

--- zsh/rc.d/91-functions.zsh
+++ zsh/rc.d/91-functions.zsh
@@ -1,19 +1,19 @@
 pcd() {
-  local project_dir=$(ghq list --full-path | peco --query "${LBUFFER}" --layout=bottom-up)
-  if [[ -n "${project_dir}" ]]; then
-      cd "${project_dir}"
-  fi
+    local project_dir=$(ghq list --full-path | peco --query "${LBUFFER}" --layout=bottom-up)
+    if [[ -n "${project_dir}" ]]; then
+        cd "${project_dir}"
+    fi
 }
 
 fcd() {
-  if [[ -n "$1" ]]; then
-      base_dir="$1"
-  else
-      base_dir=$(PWD)
-  fi
-  target_dir=$(find ${base_dir} -type d | peco --query "${LBUFFER}" --layout=bottom-up | xargs realpath)
-  if [[ -n "${target_dir}" ]]; then
-      cd ${target_dir}
-      print -s "cd ${target_dir}"
-  fi
+    if [[ -n "$1" ]]; then
+        base_dir="$1"
+    else
+        base_dir=$(PWD)
+    fi
+    target_dir=$(find ${base_dir} -type d | peco --query "${LBUFFER}" --layout=bottom-up | xargs realpath)
+    if [[ -n "${target_dir}" ]]; then
+        cd ${target_dir}
+        print -s "cd ${target_dir}"
+    fi
 }
