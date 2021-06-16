5f9f2f3f99e4f7ddcdaa80c23ad6b81dab4cc089
Ryota Kota <ryota.kota@member.fsf.org>
2021-06-16 14:22:41 +0900

    add asciinema as a bundled pip package

---
+++ lib/pip-bundle.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+source $HOME/.dotenv
+
+# Manage a package and its dependencies in the version-specific directory
+[ -z "$PKG_NAME" ] && echo "Please set PKG_NAME" && exit 1
+pkg_name="$PKG_NAME"
+pkg_version="${PKG_VERSION:-latest}"
+
+pkg_path="${DOT_CACHE_DIR}/pip/$pkg_name/${pkg_version}"
+
+cmd="${CMD:-$pkg_name}"
+
+if [ ! -e "$pkg_path" -o "$PKG_UPDATE" = true ]; then
+    if [ "$pkg_version" == "latest" ]; then
+        pkg_spec=$pkg_name
+    else
+        pkg_spec="$pkg_name==$pkg_version"
+    fi
+    pip install -U pip
+    pip install -U -t $pkg_path $pkg_spec
+fi
+
+env PYTHONPATH=$pkg_path $pkg_path/bin/$cmd $@
