cbdca2d51e7da33c58a0077bf2fdec38cae4c1de
Ryota Kota <ryota.kota@member.fsf.org>
2021-08-22 07:30:29 +0900

    update LSP

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,9 +1,25 @@
+-- Leading keys to use LSP function
+local leader = '<LocalLeader>'
+
+-- Custom border of hover window
+local floating_border = {
+      {"+", "FloatBorder"},
+      {"-", "FloatBorder"},
+      {"+", "FloatBorder"},
+      {"|", "FloatBorder"},
+      {"+", "FloatBorder"},
+      {"-", "FloatBorder"},
+      {"+", "FloatBorder"},
+      {"|", "FloatBorder"},
+}
 
 local on_attach = function(client, bufnr)
+  local function lspmap(m) return leader .. m end
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
   local opts = { noremap=true, silent=true }
 
+  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border, focusable = false })
   vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
       -- Don't show message as virtual text
@@ -15,8 +31,15 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev({ popup_opts = { focusable = false } })<CR>', opts)
   buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next({ popup_opts = { focusable = false } })<CR>', opts)
 
-  -- Call hover() when holding cursor
-  -- vim.cmd('autocmd CursorHold * lua vim.lsp.buf.hover()')
+  buf_set_keymap('n', lspmap('h'), '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', lspmap('d'), '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', lspmap('D'), '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', lspmap('i'), '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  buf_set_keymap('n', lspmap('t'), '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  buf_set_keymap('n', lspmap('s'), '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', lspmap('a'), '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_set_keymap('n', lspmap('o'), '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+
   -- Highlight a symbol and its references when holding the cursor
   if client.resolved_capabilities.document_highlight then
     vim.api.nvim_exec([[
@@ -38,9 +61,9 @@ local on_attach = function(client, bufnr)
 
   -- Add `:Format` command
   if client.resolved_capabilities.document_formatting then
-    buf_set_keymap('n', '<Leader>F', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+    buf_set_keymap('n', lspmap('f'), '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
   elseif client.resolved_capabilities.document_range_formatting then
-    buf_set_keymap('n', '<Leader>F', '<cmd>lua vim.lsp.buf.range_formatting()<CR>', opts)
+    buf_set_keymap('n', lspmap('f'), '<cmd>lua vim.lsp.buf.range_formatting()<CR>', opts)
   end
 end
 
