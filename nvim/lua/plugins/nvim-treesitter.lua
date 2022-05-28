b9385f56eb691389559ca53802633ed33537abf0
Ryota Kota <ryota.kota@member.fsf.org>
2022-05-29 00:31:15 +0900

    treesitter ignores installations of phpdoc and haskell only in macOS

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,12 +1,22 @@
-require'nvim-treesitter.configs'.setup {
+local ts_config = {
   highlight = {
     enable = true,
     disable = {},
   },
   -- Install parsers automatically
   ensure_installed = 'all',
+  ignore_install = {},
   -- p00f/nvim-ts-rainbow
   rainbow = {
     enable = true,
   },
 }
+
+if jit.os == 'OSX' then
+  -- Ignore haskell until C++11 related bug in macOS is fixed.
+  table.insert(ts_config.ignore_install, 'haskell')
+  -- An error about Xcode occured on my environment
+  table.insert(ts_config.ignore_install, 'phpdoc')
+end
+
+require'nvim-treesitter.configs'.setup(ts_config)
