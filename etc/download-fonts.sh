f9bcec2494ee117369d32dc044c2bd250a2bf1f6
Ryota Kota <ryota.kota@member.fsf.org>
2023-04-16 11:10:33 +0900

    fix downloading fonts

--- etc/download-fonts.sh
+++ etc/download-fonts.sh
@@ -9,7 +9,7 @@ else
     cica_font_path="${HOME}/.local/share/fonts"
 fi
 
-curl -sfL "${CICA_RELEASES_URL}" | jq -r '.[0].assets | .[].browser_download_url' | grep with_emoji.zip | xargs -I{} curl -fL -o cica.zip "{}"
+curl -sfL "${CICA_RELEASES_URL}" | jq -r '.[0].assets | .[].browser_download_url' | grep -v without_emoji.zip | head -n1 | xargs -I{} curl -fL -o cica.zip "{}"
 unzip -d $cica_font_path cica.zip Cica-Regular.ttf Cica-RegularItalic.ttf Cica-Bold.ttf Cica-BoldItalic.ttf
 rm cica.zip
 
